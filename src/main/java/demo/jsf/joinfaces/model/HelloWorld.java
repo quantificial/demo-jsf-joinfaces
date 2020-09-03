7af7442299f2f327e562c65b8538a3a180229e57
Johnson Fu <johnson.fu@gmail.com>
2020-09-03 22:30:22 +0800

    add jsoup for html sanitizer

--- src/main/java/demo/jsf/joinfaces/model/HelloWorld.java
+++ src/main/java/demo/jsf/joinfaces/model/HelloWorld.java
@@ -10,15 +10,19 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.servlet.http.HttpSession;
 
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 @Named // use cdi managed bean
 @ViewScoped
 @Data
+@Slf4j
 public class HelloWorld implements Serializable {
 
 	private static final long serialVersionUID = 3319622763326151979L;
@@ -26,15 +30,28 @@ public class HelloWorld implements Serializable {
 	private String firstName = "default first name";
 	private String lastName = "default last name";
 	private String value = "default value";
+	
+	private String message = "";
+		
 
-
+	public void processMessage() {
+		
+		log.info("message: {}", this.message);
+		
+		String safe = Jsoup.clean(this.message, Whitelist.basic());
+		
+		log.info("jsoup: {}", safe);
+		
+		this.message = safe;		
+	}
+	
 	public String showGreeting() {
-
+		
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
 		FacesContext.getCurrentInstance().getViewRoot().setLocale(Locale.CHINESE);
 
-		return "Hello " + authentication.getName() + "!";
+		return "Hello " + authentication.getName() + "!" + " " + message;
 	}
 
 	@Autowired
